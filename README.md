4f098c1a0a0700a490f8339c1fe9ef88d973f871
Jacek Tomaszewski <jacek@jtom.me>
2014-07-17 19:37:20 +0200

    Update plugin's description

---
+++ README.md
@@ -0,0 +1,18 @@
+# File & Image CustomFields
+
+### A redmine plugin by Espeo Software.
+
+Adds 2 additional types for custom fields you can create: a file and an image.
+
+Adds ImageField and FileField to CustomField types.
+
+## Requirements
+
+1. **imagemagick** installed
+
+  You can install it just like this:
+  ```sh
+  sudo apt-get install -y imagemagick
+  # or on mac
+  brew install imagemagick
+  ```
--- init.rb
+++ init.rb
@@ -6,9 +6,9 @@ require 'espeo_file_image_custom_field/patches/acts_as_customizable_patch'
 require 'espeo_file_image_custom_field/patches/custom_field_value_patch'
 
 Redmine::Plugin.register :espeo_file_image_custom_field do
-  name 'Espeo File Image Custom Field plugin'
+  name 'File & Image CustomField plugin'
   author 'espeo@jtom.me'
-  description 'This is a plugin for Redmine'
+  description 'Adds 2 additional types for custom fields you can create: a file and an image.'
   version '1.0.0'
   url 'http://espeo.pl'
   author_url 'http://jtom.me'
